6b5695941fb4ab16b0576f58884a79dfb70b1eb5
Dominik Koszkul <koszkuld@ryanair.com>
2023-08-19 12:10:21 +0200

    xml config

--- src/main/java/pl/coderslab/App.java
+++ src/main/java/pl/coderslab/App.java
@@ -1,7 +1,16 @@
 package pl.coderslab;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.coderslab.beans.HelloWorld;
+
 public class App {
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
+        HelloWorld hw1 = context.getBean("helloWorld", HelloWorld.class);
+        hw1.printMessage();
+
+
+        context.close();
     }
 }
